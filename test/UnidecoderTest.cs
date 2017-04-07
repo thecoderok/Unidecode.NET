e06dda7ac31643ffe2919776f979cef3eb8a0ae3
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2017-04-07 19:17:13 +0300

    Migrate to *.csproj. Support net4.5

---
+++ test/UnidecoderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Unidecode.NET.Tests
+{
+    public class UnidecoderTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal("Bei Jing ", "\u5317\u4EB0".Unidecode());
+            Assert.Equal("Rabota s kirillitsey", "Работа с кириллицей".Unidecode());
+            Assert.Equal("aouoAOUO", "äöűőÄÖŨŐ".Unidecode());
+            Assert.Equal("Hello, World!", "Hello, World!".Unidecode());
+            Assert.Equal("'\"\r\n", "'\"\r\n".Unidecode());
+            Assert.Equal("CZSczs", "ČŽŠčžš".Unidecode());
+            Assert.Equal("a", "ア".Unidecode());
+            Assert.Equal("a", "α".Unidecode());
+            Assert.Equal("a", "а".Unidecode());
+            Assert.Equal("chateau", "ch\u00e2teau".Unidecode());
+            Assert.Equal("vinedos", "vi\u00f1edos".Unidecode());
+        }
+    }
+}
