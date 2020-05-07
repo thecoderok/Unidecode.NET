aab04b6b278825093e168c87572670782b6f452e
Sergey Kuznetsov <cmt@phnx47.net>
2020-05-08 02:37:00 +0700

    fix: Tests

--- test/UnidecoderTest.cs
+++ test/UnidecoderTest.cs
@@ -48,7 +48,7 @@ namespace Unidecode.NET.Tests
         public void CharUnidecodeTest()
         {
             const string input = "а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я";
-            const string expected = "a b v g d e io zh z i i k l m n o p r s t u f kh ts ch sh shch ' y ' e iu ia A B V G D E Yo Zh Z I Y K L M N O P R S T U F Kh Ts Ch Sh Shch ' Y ' E Iu Ia";
+            const string expected = "a b v g d e io zh z i i k l m n o p r s t u f kh ts ch sh shch ' y ' e iu ia A B V G D E Io Zh Z I I K L M N O P R S T U F Kh Ts Ch Sh Shch ' Y ' E Iu Ia";
 
             var sb = new StringBuilder(expected.Length);
             foreach (var c in input)
